96c6855170a9543b077f7f496e4ef290ac785b4f
James Hayward <james.hayward6@icloud.com>
2019-04-16 15:57:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop() {
     return "I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."
   }
 
-
+forLoop();
 
 
 //    if (i == 1) { //if you put just one = you are equating not comparing and the loop will be infinite
