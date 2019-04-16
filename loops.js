9d14da3cb9f5afbf6d076bb132cfee0ed0f0821e
James Hayward <james.hayward6@icloud.com>
2019-04-16 15:53:35 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-function makeArray() {
+function forLoop() {
   for (let i = 0; i < 25; i++) {
     if (i == 1) { //if you put just one = you are equating not comparing and the loop will be infinite
       console.log("I am ${i} strange loop");
