8d653f113d89f8fc97c55a373bee881b0948c2ce
James Hayward <james.hayward6@icloud.com>
2019-04-16 15:52:17 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,8 +1,12 @@
-for (let i = 0; i < 25; i++) {
-  if (i == 1) { //if you put just one = you are equating not comparing and the loop will be infinite
-    console.log("I am ${i} strange loop");
-  }
-    else {
-      console.log("I am `${i}` strange loops}.");
+function makeArray() {
+  for (let i = 0; i < 25; i++) {
+    if (i == 1) { //if you put just one = you are equating not comparing and the loop will be infinite
+      console.log("I am ${i} strange loop");
+    }
+      else {
+        console.log("I am `${i}` strange loops}.");
+    }
   }
 }
+
+
