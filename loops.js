a34853e192e77e114b125f1b8694d88b5a91dea3
James Hayward <james.hayward6@icloud.com>
2019-04-16 15:12:38 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 for (let i = 0; i < 25; i++) {
-  if (i == 1) {
+  if (i == 1) { //if you put just one = you are equating not comparing and the loop will be infinite
     console.log("I am ${i} strange loop");
   }
     else {
