746c24458e2232b6ce5a791a7a1de70167b6e092
James Hayward <james.hayward6@icloud.com>
2019-04-16 17:01:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -33,7 +33,7 @@ function doWhileLoop(num) {
 function doWhileLoop(num) {
   do {
     console.log("I run once regardless.")
-  } while (incrementVariable() < 10)
+  } while (incrementVariable() > 10)
 }
 
 //function doWhileLoop(num) {
